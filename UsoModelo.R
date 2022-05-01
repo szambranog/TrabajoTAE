cd9ca219019d4dfa1a4309814685681e3aa2cbfa
Esteban Rojas Henao <ext4ur@gmail.com>
2022-05-01 17:24:01 -0500

    Modelo

--- UsoModelo.R
+++ UsoModelo.R
@@ -5,10 +5,11 @@
 #Proposito
 #
 
-load("modeloknn.RData")
+load("modeloknnAbuelxs.RData")
 
 #Definición input
-input <- data.frame(edad_casa=3.5,dist_mrt=5:1600,cant_tiendas=3)
+input <- data.frame(P6020=1, P6040=72, P5502=5, P6071=1, P6080=6, num_ninxs=2, num_adultxs=1, num_abuelxs=2,
+                    P6160=1, P8587=3, P1930=1, afiliado=1)
 input_preprocesado <- scale(input, center = media_total,
                             scale = desv_est_total)
-output <- predict(modelo_knn_k4,newdata = input_preprocesado)
+output <- predict(modelo_knn_A_k4 ,newdata = input_preprocesado)
